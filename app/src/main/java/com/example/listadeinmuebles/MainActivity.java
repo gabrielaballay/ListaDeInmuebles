cd41e4c206dff9aa46acffec58a616f371eedcd7
gabriel <aballaygabriel@gmail.com>
2019-09-08 23:35:46 -0300

    Ultimo Commit

--- app/src/main/java/com/example/listadeinmuebles/MainActivity.java
+++ app/src/main/java/com/example/listadeinmuebles/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.listadeinmuebles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Inmueble> lista=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargarDatos();
+        generarListView();
+    }
+
+    public void generarListView(){
+        ArrayAdapter<Inmueble> adapter=new ListaAdapter(this,R.layout.item,lista,getLayoutInflater());
+        ListView lv=findViewById(R.id.milista);
+        lv.setAdapter(adapter);
+    }
+
+    public void cargarDatos(){
+        lista.add(new Inmueble(R.drawable.casa1,"Juana Koslay",750000));
+        lista.add(new Inmueble(R.drawable.casa2,"Protrero",1250000));
+        lista.add(new Inmueble(R.drawable.casa3,"La Punta",550000));
     }
 }
